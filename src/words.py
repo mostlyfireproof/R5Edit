7bb14a5ea2fd1e77a09c745f929f69de33f1c10d
mostlyfireproof <iranoutofemailadressideas@gmail.com>
2021-09-21 09:57:57 -0400

    long strings so i don't clutter up main.py

---
+++ src/words.py
@@ -0,0 +1,26 @@
+""" Store long strings and Squirrel functions here to avoid cluttering main.py """
+
+CREATEPROPFUNCTION = '''
+#if SERVER
+entity function CreateEditorProp(asset a, vector pos, vector ang, bool mantle = false, float fade = 2000)
+{
+	entity e = CreatePropDynamic(a,pos,ang,SOLID_VPHYSICS,15000)
+	e.kv.fadedist = fade
+	if(mantle) e.AllowMantle()
+	return e
+}
+#endif
+
+'''
+
+HEADER = '''
+#if SERVER
+void function SpawnEditorProps()
+{
+// Written by mostly fireproof. Let me know if there are any issues!
+'''
+
+FOOTER = '''
+}
+#endif
+'''
